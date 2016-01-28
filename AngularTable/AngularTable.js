912838f022e8e74ca216ec848e87472857b10c6a
Adam Simpson <adhsimps@indiana.edu>
2016-01-28 13:37:20 -0500

    Initial commit.

--- AngularTable/AngularTable.js
+++ AngularTable/AngularTable.js
@@ -1,10 +1,10 @@
-visualizationFunctions.HeatMap = function(element, data, opts) {
+visualizationFunctions.AngularTable = function(element, data, opts) {
 	var network = visualizations[opts.ngIdentifier];
 	network.parentVis = visualizations[opts.ngComponentFor];
 	network.config = network.CreateBaseConfig();
 	network.meta = network.config.meta;
 	network.VisFunc = function() {
-		var data = network.AngularArgs.data.get("records").data;
+		// var data = network.AngularArgs.data.get("").data;
 		Utilities.runJSONFuncs(network.config.meta, [data, network.config]);
 
 	}
