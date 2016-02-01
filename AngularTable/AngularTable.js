c5a06b085d97ee3c00d4b43392980c5a9dd4f7f9
adhsimps <simpsoah@gmail.com>
2016-02-01 10:57:41 -0500

    Update AngularTable.js
    Added body. Not sure why it was excluded from the last commit.

--- AngularTable/AngularTable.js
+++ AngularTable/AngularTable.js
@@ -7,6 +7,34 @@ visualizationFunctions.AngularTable = function(element, data, opts) {
 		// var data = network.AngularArgs.data.get("").data;
 		Utilities.runJSONFuncs(network.config.meta, [data, network.config]);
 
+
+		var $injector = angular.injector(['ng', 'app']);
+		$injector.invoke(function($rootScope, $compile) {
+
+		
+			var container = document.createElement("div");
+			container.id = "table-container";
+			container.className += "table-container"
+			var string  = '<div ng-controller="basicExampleCtrl">'
+			string +='<table class="table table-striped" at-table at-paginated at-list="list" at-config="config">';
+			string += 	'<thead></thead>'
+			string += 	'<tbody>'
+			string += 		'<tr>'
+			string += 			'<td at-implicit at-sortable at-attribute="id" width="150" at-initial-sorting="asc"></td>'
+			string += 			'<td at-implicit at-sortable at-attribute="name"  width="250"></td>'
+			string += 			'<td at-implicit at-sortable at-attribute="birthdate"></td>'
+			string += 		'</tr>'
+			string += 	'</tbody>'
+			string += '</table>'
+			string += '<at-pagination at-list="list" at-config="config"></at-pagination>'
+			string += '</div>'
+			$(container).html($compile(string)($rootScope))
+			$(element).append(container);
+		});
+
+
+		console.log("I'm out here!")
+
 	}
 	return network;
 }
