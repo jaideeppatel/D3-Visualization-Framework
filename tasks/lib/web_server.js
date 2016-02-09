f45147c2d17cee52685d75b4fcf6c5d576d4d4cd
Adam Simpson <adhsimps@indiana.edu>
2016-02-09 11:33:09 -0500

    Minor changes during code review.

--- tasks/lib/web_server.js
+++ tasks/lib/web_server.js
@@ -1,9 +1,11 @@
-module.exports = {
-	options: {
-		cors: true,
-		port: 8000,
-		nevercache: true,
-		logRequests: true
-	},
-	foo: 'bar' // For some reason an extra key with a non-object value is necessary 
+module.exports = function(grunt) {
+    return {
+        options: {
+            cors: true,
+            port: lkajfdhglskdjfhg,
+            nevercache: true,
+            logRequests: true
+        },
+        foo: 'bar' // For some reason an extra key with a non-object value is necessary 
+    }
 }
