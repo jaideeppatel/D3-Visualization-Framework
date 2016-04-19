581294d8bc9c45911de03efb85543f8e7283ce82
Adam Simpson <adhsimps@indiana.edu>
2016-04-19 11:15:11 -0400

    Replaced mouseover tooltips with click event. It's a lot less jittery now.

--- D3ProportionalSymbol/D3ProportionalSymbol.js
+++ D3ProportionalSymbol/D3ProportionalSymbol.js
@@ -3,18 +3,42 @@ visualizationFunctions.D3ProportionalSymbol = function(element, data, opts) {
     network.parentVis = visualizations[opts.ngComponentFor];
     network.config = network.CreateBaseConfig();
     network.SVG = network.config.easySVG(element[0])
-        .attr('background', 'white')
-        .attr('class', 'canvas ' + opts.ngIdentifier)
+        .attr("background", "white")
+        .attr("class", "canvas " + opts.ngIdentifier)
         .style("overflow", "scroll")
-        .attr('transform', 'translate(' + (network.config.margins.left) + ',' + (network.config.margins.top) + ')')
+        .attr("transform", "translate(" + (network.config.margins.left) + "," + (network.config.margins.top) + ")")
 
     network.VisFunc = function() {
         var shapeData = usShapeData;
-        var projection = d3.geo.albersUsa()
-            .scale(800)
+        network.SVG.projection = d3.geo.albersUsa()
+            .scale(700)
             .translate([network.config.dims.fixedWidth / 2, network.config.dims.fixedHeight / 2])
+        var tip = d3.tip()
+            .attr("class", "d3-tip")
+            .offset([-10, 0])
+            .html(function(d) {
+                var headers = "<th>alt_name</th><th>lat</th><th>lng</th>";
+                var body = ""
+                d.values.children.forEach(function(d1, i1) {
+                    body += "<td>" + d1.alt_name + "</td>" + "<td>" + d1.lat + "</td>" + "<td>" + d1.lng + "</td>"
+                })
+
+                return tipHTML.replace("%d", d.key).replace("%d", headers).replace("%d", body)
+            })
+        var background = network.SVG.append("rect")
+            .attr("opacity", .000001)
+            .attr("width", "100%")
+            .attr("height", "100%")
+            .attr("x", 0)
+            .attr("y", 0)
+            .on("click", function() {
+                network.SVG.nodeG.property("clicked", "false");
+                tip.hide();
+            })
+        network.SVG.call(tip);
+
         var path = d3.geo.path()
-            .projection(projection)
+            .projection(network.SVG.projection)
         states = topojson.feature(shapeData, shapeData.objects.states).features
 
         network.SVG.g = network.SVG.append("g")
@@ -31,7 +55,7 @@ visualizationFunctions.D3ProportionalSymbol = function(element, data, opts) {
 
         var tipHTML = "";
         $.ajax({
-            url: 'templates/popup-template.html',
+            url: "templates/popup-template.html",
             success: function(res) {
                 tipHTML = res;
             },
@@ -40,39 +64,34 @@ visualizationFunctions.D3ProportionalSymbol = function(element, data, opts) {
             }
         })
 
-        var tip = d3.tip()
-            .attr('class', 'd3-tip')
-            .offset([0, 0])
-            .html(function(d) {
-                // return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
-                var headers = "<th>alt_name</th><th>lat</th><th>lng</th>";
-                var body = ""
-                d.values.children.forEach(function(d1, i1) {
-                    body += "<td>" + d1.alt_name + "</td>" + "<td>" + d1.lat + "</td>" + "<td>" + d1.lng + "</td>"
-                })
-
-                return tipHTML.replace("%d", d.key).replace("%d", headers).replace("%d", body)
-            })
-        network.SVG.call(tip);
 
         network.update = function(filteredData) {
             try { network.SVG.nodeG.selectAll("*").remove(); } catch (e) {};
             network.SVG.nodeG = network.SVG.g.selectAll(".nodeG")
-                .append("g")
                 .data(filteredData)
                 .enter()
                 .append("g")
+                .attr("class", "node")
                 .attr("transform", function(d, i) {
                     var arr = [d.values.lng, d.values.lat]
-                    var proj = projection(arr)
+                    var proj = network.SVG.projection(arr)
                     if (proj == null) {
                         d3.select(this).remove()
                     } else {
-                        return "translate(" + (proj[0] - 4.25) + "," + (proj[1] + 4.25) + ")"
+                        return "translate(" + (proj[0]) + "," + (proj[1]) + ")"
                     }
                 })
-                .on('mouseover', tip.show)
-                .on('mouseout', tip.hide)
+                .property("clicked", false)
+                .on("click", function(d, i) {
+                    if (d3.select(this).property("clicked") == true) {
+                        d3.select(this).property("clicked", false)
+                        return tip.hide(d)
+                    } else {
+                        d3.select(this).property("clicked", true)
+                        return tip.show(d)
+                    }
+
+                })
             network.SVG.nodes = network.SVG.nodeG
                 .append("circle")
                 .classed("n", true)
@@ -80,11 +99,13 @@ visualizationFunctions.D3ProportionalSymbol = function(element, data, opts) {
                 .attr("cx", 0)
                 .attr("cy", 0)
         }
-
         network.update(network.filteredData.records.data[network.currCategory])
         var centered;
 
+
         function clicked(d) {
+            network.SVG.nodeG.property("clicked", "false");
+            tip.hide();
             var x, y, k;
 
             if (d && centered !== d) {
